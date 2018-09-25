3143e5a6487577f121d4fb97241ffa883eb7c944
Ömer Yalçın <omeryalcin48@gmail.com>
2018-09-25 04:48:54 +0300

    Update 3d gaus.py

--- 3d gaus.py
+++ 3d gaus.py
@@ -3,15 +3,14 @@ import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 from mpl_toolkits.mplot3d import Axes3D
 
-#Parameters to set
+#parametreleri ayarlanması
 mu_x = 0
 variance_x = 3
-
 mu_y = 0
 variance_y = 6
 
 
-#Create grid and multivariate normal
+#grid ve çoklu gaus grafiği
 x = np.linspace(-10,10,200)
 y = np.linspace(-10,10,200)
 X, Y = np.meshgrid(x,y)
@@ -19,7 +18,7 @@ pos = np.empty(X.shape + (2,))
 pos[:, :, 0] = X; pos[:, :, 1] = Y
 rv = multivariate_normal([mu_x, mu_y], [[variance_x, 0], [0, variance_y]])
 
-
+# çizim kısmı
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 ax.plot_surface(X, Y, rv.pdf(pos),cmap='hot',linewidth=0)
